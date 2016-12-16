daafdb2275652b8cc4b870008d9c305ec68c304e
Serhiy Perevoznyk <s.perevoznyk@gmail.com>
2016-12-16 12:24:37 +0100

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System.Security.Permissions;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Karna.Tetris")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Serhiy Perevoznyk")]
+[assembly: AssemblyProduct("Tetris Game")]
+[assembly: AssemblyCopyright("Copyright © Serhiy Perevoznyk 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b808d4f-76f8-41f0-a28f-19b2301bc59e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
